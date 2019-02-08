c3033c49557b8366721689155d717b3ce2ce5a6d
MAU <42820040+manu676@users.noreply.github.com>
2019-02-07 19:17:23 -0600

    1.1
    1.1

--- progra objetos 2do/conceptos basicos/1.js
+++ progra objetos 2do/conceptos basicos/1.js
@@ -1,4 +1,21 @@
 import auto from "./auto.js"
 
-let auto1= new auto ("Ford", 2000, "rojo", 100);
-auto1.mostrarEstado();
+let auto1 = new auto ("Ford", 2000, "rojo", 100);
+let auto2 = new auto ("Nissan", 2010, "azul" );
+let auto3 =new auto ("Toyota", 2009);
+
+auto1.encender();
+auto1.avanzar (50,1.5);
+auto1.apagar();
+auto1.avanzar (50,1.5);
+console.log (`Kilometraje ${auto1.kilometraje}`);
+auto1.kilometraje = 5;
+console.log (`Kilometraje ${auto1.kilometraje}`);
+
+
+auto2.encender();
+auto2.apagar();
+
+auto3.encender();
+auto3.apagar();
+
--- progra objetos 2do/conceptos basicos/auto.js
+++ progra objetos 2do/conceptos basicos/auto.js
@@ -1,5 +1,5 @@
 export default class auto {
-    constructor(marca,modelo,color,kilometraje)
+    constructor(marca,modelo,color="blanco",kilometraje= 0)
     {
         this.marca = marca;
         this.modelo = modelo;
@@ -12,6 +12,21 @@ export default class auto {
         tiene un kilometraje de ${this.kilometraje} kms y esta ${this.estado}`);
     }
     encender(){
-
+        this.estado= "Encendido";
+        this.mostrarEstado();
+    }
+    apagar(){
+        this.estado= "Apagado";
+        this.mostrarEstado();
+    }
+    avanzar(velocidad, tiempo){
+        if (this.estado === "Apagado"){
+            console.log("El auto no puede avanzar");
+        } 
+        else{
+            let distancia = velocidad * tiempo ;
+            this.kilometraje = this.kilometraje + distancia ;
+        }
+        this.mostrarEstado();
     }
 }
